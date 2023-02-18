a9f5063ae0c7830a355e57beb2fb812d38e23760
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-02-18 21:55:41 +0600

    rectangle final result got

--- Js/geometry.js
+++ Js/geometry.js
@@ -6,12 +6,22 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
     const inputTriangleH = document.getElementById('input-h-triangle');
     const getInputTriangleValueH = inputTriangleH.value;
 
-    if (getInputTriangleValueB.value === isNaN) {
-        return alert('Please enter any valid number');
-    }
-
     const calculateTriangle = (0.5 * getInputTriangleValueB * getInputTriangleValueH).toFixed(2);
 
     const showResultTriangle = document.getElementById('result-of-triangle');
     showResultTriangle.innerText = calculateTriangle;
+});
+
+
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    const inputRectangleW = document.getElementById('input-field-rectangle-w');
+    const getInputRectangleValueW = inputRectangleW.value;
+
+    const inputRectangleI = document.getElementById('input-field-rectangle-I');
+    const getInputRectangleValueI = inputRectangleI.value;
+
+    const calculatorRectangle = (getInputRectangleValueW * getInputRectangleValueI).toFixed(2);
+
+    const showResultRectangle = document.getElementById('result-of-rectangle');
+    showResultRectangle.innerText = calculatorRectangle;
 });
