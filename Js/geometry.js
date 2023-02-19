242268b6449f0406175024bd13d34293dc0eb475
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-02-19 17:18:38 +0600

    random background color set

--- Js/geometry.js
+++ Js/geometry.js
@@ -1,3 +1,38 @@
+// Set random background color
+function randomBackgroundCardColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+}
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-1').style.backgroundColor = randomBackgroundCardColor();
+});
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-2').style.backgroundColor = randomBackgroundCardColor();
+});
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-3').style.backgroundColor = randomBackgroundCardColor();
+});
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-4').style.backgroundColor = randomBackgroundCardColor();
+});
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-5').style.backgroundColor = randomBackgroundCardColor();
+});
+
+document.addEventListener('mouseover', function(){
+    document.getElementById('random-bg-color-6').style.backgroundColor = randomBackgroundCardColor();
+});
+
+
+
 document.getElementById('btn-triangle').addEventListener('click', function () {
     const inputTriangleB = document.getElementById('input-b-triangle');
     const getInputTriangleValueB = inputTriangleB.value;
@@ -10,10 +45,19 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
 
     const showResultTriangle = document.getElementById('result-of-triangle');
     showResultTriangle.innerText = calculateTriangle;
+
+    const tableContainer = document.getElementById('calculation-area');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${1}</td>
+        <td>${Triangle}</td>
+        <td>${calculateTriangle}</td>
+    `
+    tableContainer.appendChild('tr');
 });
 
 
-document.getElementById('btn-rectangle').addEventListener('click', function(){
+document.getElementById('btn-rectangle').addEventListener('click', function () {
     const inputRectangleW = document.getElementById('input-field-rectangle-w');
     const getInputRectangleValueW = inputRectangleW.value;
 
@@ -29,10 +73,10 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
 
 
 
-document.getElementById('btn-parallelogram').addEventListener('click', function(){
+document.getElementById('btn-parallelogram').addEventListener('click', function () {
     const b = 10;
     const h = 12;
-    
+
     const calculatorParallelogram = b * h;
 
     const showResultParallelogram = document.getElementById('result-of-parallelogram');
@@ -42,7 +86,7 @@ document.getElementById('btn-parallelogram').addEventListener('click', function(
 
 
 
-document.getElementById('btn-Rhombus').addEventListener('click', function(){
+document.getElementById('btn-Rhombus').addEventListener('click', function () {
     const d1 = 16;
     const d2 = 8;
 
