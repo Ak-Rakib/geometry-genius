3f3c58d3718f0d9b7ccfecd7d210f982bf1c5ed7
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-02-20 10:37:39 +0600

    add validation

--- Js/geometry.js
+++ Js/geometry.js
@@ -7,36 +7,38 @@ function randomBackgroundCardColor() {
     return 'rgb(' + color.join(', ') + ')';
 }
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-1').style.backgroundColor = randomBackgroundCardColor();
 });
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-2').style.backgroundColor = randomBackgroundCardColor();
 });
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-3').style.backgroundColor = randomBackgroundCardColor();
 });
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-4').style.backgroundColor = randomBackgroundCardColor();
 });
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-5').style.backgroundColor = randomBackgroundCardColor();
 });
 
-document.addEventListener('mouseover', function(){
+document.addEventListener('mouseover', function () {
     document.getElementById('random-bg-color-6').style.backgroundColor = randomBackgroundCardColor();
 });
+
+
 
 
 let serial = 0;
 
 // Get result Triangle
 document.getElementById('btn-triangle').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const inputTriangleB = document.getElementById('input-b-triangle');
     const getInputTriangleValueB = inputTriangleB.value;
 
@@ -44,11 +46,15 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
     const inputTriangleH = document.getElementById('input-h-triangle');
     const getInputTriangleValueH = inputTriangleH.value;
 
-    const calculatorTriangle = (0.5 * getInputTriangleValueB * getInputTriangleValueH).toFixed(2);
-    dataOfTriangle(serial, calculatorTriangle);
+    if (isNaN(getInputTriangleValueB) || isNaN(getInputTriangleValueH)) {
+        return alert('Please use a number');
+    } else {
+        const calculatorTriangle = (0.5 * getInputTriangleValueB * getInputTriangleValueH).toFixed(2);
+        dataOfTriangle(serial, calculatorTriangle);
+    }
 });
 
-function dataOfTriangle(serial, calculatorTriangle){
+function dataOfTriangle(serial, calculatorTriangle) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -63,19 +69,22 @@ function dataOfTriangle(serial, calculatorTriangle){
 
 // get result Rectangle
 document.getElementById('btn-rectangle').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const inputRectangleW = document.getElementById('input-field-rectangle-w');
     const getInputRectangleValueW = inputRectangleW.value;
 
     const inputRectangleI = document.getElementById('input-field-rectangle-I');
     const getInputRectangleValueI = inputRectangleI.value;
 
-    const calculatorRectangle = (getInputRectangleValueW * getInputRectangleValueI).toFixed(2);
-    dataOfRectangle(serial, calculatorRectangle)
-    
+    if (isNaN(getInputRectangleValueW) || isNaN(getInputRectangleValueI)) {
+        return alert('Please use a number');
+    } else {
+        const calculatorRectangle = (getInputRectangleValueW * getInputRectangleValueI).toFixed(2);
+        dataOfRectangle(serial, calculatorRectangle)
+    }
 });
 
-function dataOfRectangle(serial, calculatorRectangle){
+function dataOfRectangle(serial, calculatorRectangle) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -91,7 +100,7 @@ function dataOfRectangle(serial, calculatorRectangle){
 
 // get result Parallelogram
 document.getElementById('btn-parallelogram').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const b = 10;
     const h = 12;
 
@@ -99,7 +108,7 @@ document.getElementById('btn-parallelogram').addEventListener('click', function 
     dataOfParallelogram(serial, calculatorParallelogram);
 });
 
-function dataOfParallelogram(serial, calculatorParallelogram){
+function dataOfParallelogram(serial, calculatorParallelogram) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -115,7 +124,7 @@ function dataOfParallelogram(serial, calculatorParallelogram){
 
 // Get result of Rhombus
 document.getElementById('btn-Rhombus').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const d1 = 16;
     const d2 = 8;
 
@@ -124,7 +133,7 @@ document.getElementById('btn-Rhombus').addEventListener('click', function () {
 
 });
 
-function dataOfRhombus(serial, calculatorRhombus){
+function dataOfRhombus(serial, calculatorRhombus) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -133,14 +142,14 @@ function dataOfRhombus(serial, calculatorRhombus){
         <td>${calculatorRhombus}cm<sup>2</sup></td>
         <td><button class='btn btn-outline-success'>Convert to m<sup>2</sup></button></td>
     `;
-    tableContainer.appendChild(tr); 
+    tableContainer.appendChild(tr);
 }
 
 
 
 // Get result of Pentagon
 document.getElementById('btn-pentagon').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const p = 6;
     const b = 10;
 
@@ -149,7 +158,7 @@ document.getElementById('btn-pentagon').addEventListener('click', function () {
 
 });
 
-function dataOfPentagon(serial, calculatorPentagon){
+function dataOfPentagon(serial, calculatorPentagon) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -158,14 +167,14 @@ function dataOfPentagon(serial, calculatorPentagon){
         <td>${calculatorPentagon}cm<sup>2</sup></td>
         <td><button class='btn btn-outline-success'>Convert to m<sup>2</sup></button></td>
     `;
-    tableContainer.appendChild(tr); 
+    tableContainer.appendChild(tr);
 }
 
 
 
 // Get result of Ellipse
 document.getElementById('btn-ellipse').addEventListener('click', function () {
-    serial +=1;
+    serial += 1;
     const b = 10;
     const h = 12;
 
@@ -174,7 +183,7 @@ document.getElementById('btn-ellipse').addEventListener('click', function () {
 
 });
 
-function dataOfEllipse(serial, calculatorEllipse){
+function dataOfEllipse(serial, calculatorEllipse) {
     const tableContainer = document.getElementById('calculation-area');
     const tr = document.createElement('tr');
     tr.innerHTML = `
@@ -183,5 +192,5 @@ function dataOfEllipse(serial, calculatorEllipse){
         <td>${calculatorEllipse}cm<sup>2</sup></td>
         <td><button class='btn btn-outline-success'>Convert to m<sup>2</sup></button></td>
     `;
-    tableContainer.appendChild(tr); 
+    tableContainer.appendChild(tr);
 }
